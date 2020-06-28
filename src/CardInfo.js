15827eaf8021e5b419996a7f171c97123734d3d2
sshrik <chj8781942naver.com>
2020-06-29 00:24:29 +0900

    Add Image and Image source to card info js file.

--- src/CardInfo.js
+++ src/CardInfo.js
@@ -18,241 +18,241 @@ export const TokenCardList = {
             need : [1, 1, 1, 1, 0],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [1, 2, 1, 1, 0],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [2, 2, 0, 1, 0],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [0, 0, 1, 3, 1],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [0, 0, 2, 1, 0],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [2, 0, 2, 0, 0],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [0, 0, 3, 0, 0],
             reword : 4,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [0, 4, 0, 0, 0],
             reword : 4,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 5.png"
         },
         {
             need : [1, 0, 1, 1, 1],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [1, 0, 1, 2, 1],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [1, 0, 2, 2, 0],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [0, 1, 3, 1, 0],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [1, 0, 0, 0, 2],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [0, 0, 2, 0, 2],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [0, 0, 0, 0, 3],
             reword : 1,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [0, 0, 0, 4, 0],
             reword : 1,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 2.png"
         },
         {
             need : [0, 1, 1, 1, 1],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [0, 1, 2, 1, 1],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [0, 2, 2, 0, 1],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [3, 1, 0, 0, 1],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [0, 0, 0, 2, 1],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [0, 2, 0, 0, 2],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [0, 3, 0, 0, 0],
             reword : 0,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [0, 0, 4, 0, 0],
             reword : 0,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 1.png"
         },
         {
             need : [1, 1, 0, 1, 1],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [1, 1, 0, 1, 2],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [0, 1, 0, 2, 2],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [1, 3, 1, 0, 0],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [2, 1, 0, 0, 0],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [0, 2, 0, 2, 0],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [0, 0, 0, 3, 0],
             reword : 2,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [0, 0, 0, 0, 4],
             reword : 2,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 3.png"
         },
         {
             need : [1, 1, 1, 0, 1],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [2, 1, 1, 0, 1],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [2, 0, 1, 0, 2],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [1, 0, 0, 1, 3],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [0, 2, 1, 0, 0],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [2, 0, 0, 2, 0],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [3, 0, 0, 0, 0],
             reword : 3,
             score: 0,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
         {
             need : [4, 0, 0, 0, 0],
             reword : 3,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 1 4.png"
         },
     ],
     tier2 : [
@@ -260,181 +260,181 @@ export const TokenCardList = {
             need : [3, 2, 2, 0, 0],
             reword : 4,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 9.png"
         },
         {
             need : [3, 0, 3, 0, 2],
             reword : 4,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 9.png"
         },
         {
             need : [0, 1, 4, 2, 0],
             reword : 4,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 9.png"
         },
         {
             need : [0, 0, 5, 3, 0],
             reword : 4,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 10.png"
         },
         {
             need : [5, 0, 0, 0, 0],
             reword : 4,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 10.png"
         },
         {
             need : [0, 0, 0, 0, 6],
             reword : 4,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 10.png"
         },
         {
             need : [0, 2, 2, 3, 0],
             reword : 1,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 3.png"
         },
         {
             need : [0, 2, 3, 0, 3],
             reword : 1,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 3.png"
         },
         {
             need : [5, 3, 0, 0, 0],
             reword : 1,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 3.png"
         },
         {
             need : [2, 0, 0, 1, 4],
             reword : 1,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 4.png"
         },
         {
             need : [0, 5, 0, 0, 0],
             reword : 1,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 4.png"
         },
         {
             need : [0, 6, 0, 0, 0],
             reword : 1,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 4.png"
         },
         {
             need : [0, 0, 3, 2, 2],
             reword : 0,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 1.png"
         },
         {
             need : [2, 3, 0, 3, 0],
             reword : 0,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 1.png"
         },
         {
             need : [0, 0, 1, 4, 2],
             reword : 0,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 1.png"
         },
         {
             need : [0, 0, 0, 5, 3],
             reword : 0,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 2.png"
         },
         {
             need : [0, 0, 0, 5, 0],
             reword : 0,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 2.png"
         },
         {
             need : [6, 0, 0, 0, 0],
             reword : 0,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 2.png"
         },
         {
             need : [3, 0, 2, 3, 0],
             reword : 2,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 5.png"
         },
         {
             need : [2, 3, 0, 0, 2],
             reword : 2,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 5.png"
         },
         {
             need : [4, 2, 0, 0, 1],
             reword : 2,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 5.png"
         },
         {
             need : [0, 5, 3, 0, 0],
             reword : 2,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 6.png"
         },
         {
             need : [0, 0, 5, 0, 0],
             reword : 2,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 6.png"
         },
         {
             need : [0, 0, 6, 0, 0],
             reword : 2,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 6.png"
         },
         {
             need : [2, 0, 0, 2, 3],
             reword : 3,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 7.png"
         },
         {
             need : [0, 3, 0, 2, 3],
             reword : 3,
             score: 1,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 7.png"
         },
         {
             need : [1, 4, 2, 0, 0],
             reword : 3,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 7.png"
         },
         {
             need : [3, 0, 0, 0, 5],
             reword : 3,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 8.png"
         },
         {
             need : [0, 0, 0, 0, 5],
             reword : 3,
             score: 2,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 8.png"
         },
         {
             need : [0, 0, 0, 6, 0],
             reword : 3,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 2 8.png"
         },
     ],
     tier3 : [
@@ -442,121 +442,121 @@ export const TokenCardList = {
             need : [3, 3, 5, 3, 0],
             reword : 4,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 9.png"
         },
         {
             need : [0, 0, 0, 7, 0],
             reword : 4,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 9.png"
         },
         {
             need : [0, 0, 3, 6, 3],
             reword : 4,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 10.png"
         },
         {
             need : [0, 0, 0, 7, 3],
             reword : 4,
             score: 5,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 10.png"
         },
         {
             need : [3, 0, 3, 3, 5],
             reword : 1,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 3.png"
         },
         {
             need : [7, 0, 0, 0, 0],
             reword : 1,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 3.png"
         },
         {
             need : [6, 3, 0, 0, 3],
             reword : 1,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 4.png"
         },
         {
             need : [7, 3, 0, 0, 0],
             reword : 1,
             score: 5,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 4.png"
         },
         {
             need : [0, 3, 3, 5, 3],
             reword : 0,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 1.png"
         },
         {
             need : [0, 0, 0, 0, 7],
             reword : 0,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 1.png"
         },
         {
             need : [3, 0, 0, 3, 6],
             reword : 0,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 2.png"
         },
         {
             need : [3, 0, 0, 0, 7],
             reword : 0,
             score: 5,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 2.png"
         },
         {
             need : [5, 3, 0, 3, 3],
             reword : 2,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 5.png"
         },
         {
             need : [0, 7, 0, 0, 0],
             reword : 2,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 5.png"
         },
         {
             need : [3, 6, 3, 0, 0],
             reword : 2,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 6.png"
         },
         {
             need : [0, 7, 3, 0, 0],
             reword : 2,
             score: 5,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 6.png"
         },
         {
             need : [3, 5, 3, 0, 3],
             reword : 3,
             score: 3,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 7.png"
         },
         {
             need : [0, 0, 7, 0, 0],
             reword : 3,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 7.png"
         },
         {
             need : [0, 3, 6, 3, 0],
             reword : 3,
             score: 4,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 8.png"
         },
         {
             need : [0, 0, 7, 3, 0],
             reword : 3,
             score: 5,
-            imgSrc : ""
+            imgSrc : "resource/tokenCard/Tier 3 8.png"
         },
     ],
 };
@@ -565,53 +565,60 @@ export const ComboCardList = [
     {
         need : [4, 0, 0, 0, 4],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 1.png"
     },
     {
         need : [4, 4, 0, 0, 0],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 2.png"
     },
     {
         need : [3, 3, 3, 0, 0],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 3.png"
     },
     {
         need : [0, 0, 0, 4, 4],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 4.png"
     },
     {
         need : [0, 0, 3, 3, 3],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 5.png"
     },
     {
         need : [0, 4, 4, 0, 0],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 6.png"
     },
     {
         need : [3, 0, 0, 3, 3],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 7.png"
     },
     {
         need : [0, 3, 3, 3, 0],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 8.png"
     },
     {
         need : [0, 0, 4, 4, 0],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 9.png"
     },
     {
         need : [3, 3, 0, 0, 3],
         score: 3,
-        imgSrc : ""
+        imgSrc : "resource/comboCard/Combo Card 10.png"
     }
 ];
 
-export const TokenImageList = ["", "", "", "", "", ""];
+export const TokenImageList = [
+    "resource/token/Token 1", 
+    "resource/token/Token 2", 
+    "resource/token/Token 3", 
+    "resource/token/Token 4", 
+    "resource/token/Token 5", 
+    "resource/token/Token 6"
+];
