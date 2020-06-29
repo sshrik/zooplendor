615989817e4f2d2fce5eb1147c982ff90726f10b
sshrik <chj8781942naver.com>
2020-06-29 18:42:12 +0900

    Add Combo component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
 
 function App() {
 	return (
-		<div>
+		<div style={{ flex: 1, flexDirection: "row" }}>
 			<ComboCard CardInfo={ ComboCardList[0] }/>
 			<ComboCard CardInfo={ ComboCardList[1] }/>
 			<ComboCard CardInfo={ ComboCardList[2] }/>
@@ -22,19 +22,46 @@ function App() {
 	);
 }
 
-function ComboCard(props) {
-	var imgSrc = "./" + props.CardInfo.imgSrc;
-	console.log(props.cardInfo);
+function TokenCounter(props) {
+	var tokenImgs = [];
+	
+	for(var i = 0; i < props.CardInfo.need.length; i++) {
+		if(props.CardInfo.need[i] != 0) {
+			tokenImgs.push(
+				<span style={{ display:"flex", marginBottom: 2 }} key={i}>
+					<span style={{ width: 20, height: 20 }}>
+						<img src={ require( `${TokenImageList[i]}`) } style={{ width: 20, height: 20, resizeMode: "contain" }} />
+					</span>
+					<span style={{ width: 10, height: 20, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+						<p style={{ fontSize: 10, display: 'inline' }} >{ props.CardInfo.need[i] }</p>
+					</span>
+				</span>
+			);
+		}
+	}
+	
+	var styles = Object.assign({}, props.style, { marginTop: 10, marginLeft: 2, display: 'flex', flexDirection: "column", justifyContent: 'flex-end', alignItems: 'center' });
+	console.log(styles);
 
 	return (
-		<div style={{flex: 1}}>
-			<div style={{width: 30, height: "100%", position: "absolute", backgroundColor : "#ffffff", opacity: 0.5}}>
-				<text>{props.CardInfo.score}</text>
-			</div>
+		<span style={styles}>
+			{tokenImgs}
+		</span>
+	);
+}
+
+function ComboCard(props) {
+	return (
+		<span style={{ flex: 1 }}>
+			<span style={{ width: 30, height: 100, position:'absolute', backgroundColor : 'rgba(255, 255, 255, 0.8)' }}>
+				<p style={{ textAlign: 'center', margin: 5, fontSize: 18 }}>{props.CardInfo.score}</p>
+				<TokenCounter style={{ width: 30, height: 60 }} CardInfo={ props.CardInfo }/>
+			</span>
 			<img 
-				src={ require(`${imgSrc}`) }
+				src={ require(`${props.CardInfo.imgSrc}`) }
+				style={{ width: 100, height: 100 }}
 			/>
-		</div>
+		</span>
 	)
 }
 
