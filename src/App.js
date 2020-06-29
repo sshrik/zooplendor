fa66be2e10ab37a99a696c3834add9972136cb23
sshrik <chj8781942naver.com>
2020-06-29 20:42:05 +0900

    Refactoring Combo Card and Token Counter into another files.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import {
-	WHITE_TOKEN_INDEX, 
-	BLUE_TOKEN_INDEX, 
-	GREEN_TOKEN_INDEX, 
-	RED_TOKEN_INDEX, 
-	BLACK_TOKEN_INDEX, 
-	GOLDEN_TOKEN_INDEX, 
-	TokenCardList, 
-	ComboCardList, 
-	TokenImageList
-} from './CardInfo.js';
+import { ComboCard } from './ComboCard.js'
+import { ComboCardList } from './CardInfo.js'
 
 function App() {
 	return (
@@ -20,49 +11,6 @@ function App() {
 			<ComboCard CardInfo={ ComboCardList[3] }/>
 		</div>
 	);
-}
-
-function TokenCounter(props) {
-	var tokenImgs = [];
-	
-	for(var i = 0; i < props.CardInfo.need.length; i++) {
-		if(props.CardInfo.need[i] != 0) {
-			tokenImgs.push(
-				<span style={{ display:"flex", marginBottom: 2 }} key={i}>
-					<span style={{ width: 20, height: 20 }}>
-						<img src={ require( `${TokenImageList[i]}`) } style={{ width: 20, height: 20, resizeMode: "contain" }} />
-					</span>
-					<span style={{ width: 10, height: 20, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-						<p style={{ fontSize: 10, display: 'inline' }} >{ props.CardInfo.need[i] }</p>
-					</span>
-				</span>
-			);
-		}
-	}
-	
-	var styles = Object.assign({}, props.style, { marginTop: 10, marginLeft: 2, display: 'flex', flexDirection: "column", justifyContent: 'flex-end', alignItems: 'center' });
-	console.log(styles);
-
-	return (
-		<span style={styles}>
-			{tokenImgs}
-		</span>
-	);
-}
-
-function ComboCard(props) {
-	return (
-		<span style={{ flex: 1 }}>
-			<span style={{ width: 30, height: 100, position:'absolute', backgroundColor : 'rgba(255, 255, 255, 0.8)' }}>
-				<p style={{ textAlign: 'center', margin: 5, fontSize: 18 }}>{props.CardInfo.score}</p>
-				<TokenCounter style={{ width: 30, height: 60 }} CardInfo={ props.CardInfo }/>
-			</span>
-			<img 
-				src={ require(`${props.CardInfo.imgSrc}`) }
-				style={{ width: 100, height: 100 }}
-			/>
-		</span>
-	)
 }
 
 export default App;
---
+++ src/TokenCounter.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { TokenImageList } from './CardInfo.js';
+
+const tokenCounterStyles = {
+    tokenImageCounterContainer: {
+        display: "flex",
+        marginBottom: 2
+    },
+    tokenImageContainer : { 
+        width: 20, 
+        height: 20 
+    },
+    tokenImage : { 
+        width: 18, 
+        height: 18, 
+        resizeMode: "contain" 
+    },
+    tokenNumberContainer : { 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        width: 10, 
+        height: 20, 
+    },
+    tokenNumber : { 
+        display: 'inline',
+        fontSize: 10
+    },
+    tokenCounterContainer:  { 
+        display: 'flex',
+        flexDirection: "column", 
+        justifyContent: 'flex-end',
+        marginLeft: 1,  
+    }
+}
+
+export function TokenCounter(props) {
+	var tokenImgs = [];
+	
+	for(var i = 0; i < props.CardInfo.need.length; i++) {
+		if(props.CardInfo.need[i] != 0) {
+			tokenImgs.push(
+				<span style={ tokenCounterStyles.tokenImageCounterContainer } key={i}>
+					<span style={ tokenCounterStyles.tokenImageContainer }>
+						<img 
+                            src={ require( `${TokenImageList[i]}`) } 
+                            style={ tokenCounterStyles.tokenImage }
+                        />
+					</span>
+					<span style={ tokenCounterStyles.tokenNumberContainer }>
+						<p style={ tokenCounterStyles.tokenNumber }>
+                            { props.CardInfo.need[i] }
+                        </p>
+					</span>
+				</span>
+			);
+		}
+    }
+    
+	return (
+		<span style={ Object.assign({}, props.style, tokenCounterStyles.tokenCounterContainer) }>
+			{tokenImgs}
+		</span>
+	);
+}
