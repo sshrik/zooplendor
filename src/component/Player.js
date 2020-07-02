0f401542fec68ffe788c46780e1a5f7af8eb2a4b
sshrik <chj8781942naver.com>
2020-07-02 20:57:46 +0900

    having card now colored.

--- src/component/Player.js
+++ src/component/Player.js
@@ -15,11 +15,20 @@ function GoldenToken(props) {
         <img 
             style={Object.assign({}, props.style, { width: 30, height: 30, resizeMode: 'contain'})} 
             src={ TokenImageList[GOLDEN_TOKEN_INDEX] } 
-        />
+            />
     )
 }
 
 function HavingCard(props) {
+    return(
+        <span style={Object.assign({}, props.style, { margin: 5 })}>
+            <p style={{ fontSize: 10, textAlign: "center" }}> { props.number } </p>
+            <img style={{width:50, height: 70, resizeMode: 'contain'}} src={ ColoredBackCardImageList.tier1[props.imgIndx] }/>
+        </span>
+    )
+}
+
+function SavingCard(props) {
     return(
         <span style={Object.assign({}, props.style, { margin: 5 })}>
             <p style={{ fontSize: 10, textAlign: "center" }}> { props.number } </p>
@@ -36,9 +45,9 @@ export function Player(props) {
                 <span style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
                     <p style={{ fontSize: 20, textAlign: "center" }}>8</p>
                 </span>
-                <HavingCard img={ColoredBackCardImageList[WHITE_TOKEN_INDEX]} number={1} style={{flex: 1}} />
-                <HavingCard img={ColoredBackCardImageList[BLUE_TOKEN_INDEX]} number={1} style={{flex: 1}} />
-                <HavingCard img={ColoredBackCardImageList[GREEN_TOKEN_INDEX]} number={1} style={{flex: 1}} />
+                <HavingCard havingList={ [1, 0, 0] } imgIndx={ WHITE_TOKEN_INDEX } number={1} style={{flex: 1}} />
+                <HavingCard havingList={ [1, 0, 0] } imgIndx={ BLUE_TOKEN_INDEX } number={1} style={{flex: 1}} />
+                <HavingCard havingList={ [1, 0, 0] } imgIndx={ GREEN_TOKEN_INDEX } number={1} style={{flex: 1}} />
             </div>
             <div style={{display: "flex"}}>
                 <span style={{ display: 'flex', flex: 1, flexDirection: "column", justifyContent: "center", alignContent: 'center' }}>
@@ -46,9 +55,9 @@ export function Player(props) {
                     <GoldenToken style={{marginTop: -23}}/>
                     <GoldenToken style={{marginTop: -23}}/>
                 </span>
-                <HavingCard img={BackCardImageList[0]} number={1} style={{flex: 1}} />
-                <HavingCard img={ColoredBackCardImageList[RED_TOKEN_INDEX]} number={1} style={{flex: 1}} />
-                <HavingCard img={ColoredBackCardImageList[BLACK_TOKEN_INDEX]} number={1} style={{flex: 1}} />
+                <SavingCard img={BackCardImageList[0]} number={1} style={{flex: 1}} />
+                <HavingCard havingList={ [1, 0, 0] } imgIndx={ RED_TOKEN_INDEX } number={1} style={{flex: 1}} />
+                <HavingCard havingList={ [1, 0, 0] } imgIndx={ BLACK_TOKEN_INDEX } number={1} style={{flex: 1}} />
             </div>
         </span>
     )
